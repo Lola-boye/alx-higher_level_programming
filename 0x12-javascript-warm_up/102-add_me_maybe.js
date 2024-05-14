abc7c2ee9b2b11321323e58f5911a6af879a8474
Lola-boye <103613554+Lola-boye@users.noreply.github.com>
2024-05-14 12:46:17 +0100

    JS Projects

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+function incrementAndCallFunction(number, theFunction) {
+	  number++;
+	  theFunction(number);
+}
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+0-javascript\_is\_amazing.js - ```a script that prints “JavaScript is amazing``` 
